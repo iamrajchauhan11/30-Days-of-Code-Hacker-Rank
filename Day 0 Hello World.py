1314517d0a79d53fcb752920ee3146e6acbd870e
Suraj Chauhan <scrajput11@gmail.com>
2022-07-30 00:32:20 +0530

    Day 0 Hello World.py

---
+++ Day 0 Hello World.py
@@ -0,0 +1,12 @@
+#Day 0 Hello, World
+#In python, Hacker Rank terminal.. you will see this type of code 
+#Soo Lets start Code...
+
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+print(input_string) #<= You just need to add this line only.....
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
