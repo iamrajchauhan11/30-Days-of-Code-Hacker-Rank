96566ac39ee368e76237924ee10d5963106d7128
Suraj Chauhan <scrajput11@gmail.com>
2022-07-30 00:37:47 +0530

    Day 1 Data Types.py

---
+++ Day 1 Data Types.py
@@ -0,0 +1,28 @@
+#In Day 1 Data Types you will find this type of Code in Python  
+#Soo Let's start Coding...
+
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+#First you need to Decalre Variables:
+a = int(input())
+b = float(input())
+c = str(input())
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+#Next print the following:
+print(i+a) #<= Add this line
+
+# Print the sum of the double variables on a new line.
+#Next, print the D value and B Value
+print(d+b) #<= Add this line
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+#Concatenate the STrings
+print(s+""+c) #<= Add this line.
+
+#That's it 
