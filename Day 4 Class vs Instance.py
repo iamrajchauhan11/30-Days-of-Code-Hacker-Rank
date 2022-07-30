241a06f281ff34d98e029b696ced8f4a091153bb
Suraj Chauhan <scrajput11@gmail.com>
2022-07-30 20:40:04 +0530

    Day 4 Class vs Instance

---
+++ Day 4 Class vs Instance.py
@@ -0,0 +1,34 @@
+#Day 4 Class Vs Instance... You will find this type of Code in Pyhton...
+#Let's Start coding....
+
+class Person:
+    def __init__(self,initialAge):
+        # Add some more code to run some checks on initialAge
+        if initialAge < 0:
+            self.age = 0
+            print("Age is not valid, setting age to 0.")   #< Add This code
+        else:
+            self.age = initialAge
+
+    def amIOld(self):
+        # Do some computations in here and print out the correct statement to the console
+        if self.age < 13:
+            print("You are young.")
+        elif self.age >= 13 and self.age < 18:              #<= Add this Code
+            print("You are a teenager.")
+        else: 
+            print("You are old.")
+
+    def yearPasses(self):
+        # Increment the age of the person in here
+        self.age += 1                #<= Add this code
+
+t = int(input())
+for i in range(0, t):
+    age = int(input())         
+    p = Person(age)  
+    p.amIOld()
+    for j in range(0, 3):
+        p.yearPasses()       
+    p.amIOld()
+    print("")
