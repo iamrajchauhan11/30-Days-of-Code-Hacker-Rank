537b64617fcface9d51b2f6ed6f8c85a4f8b3e5b
Suraj Chauhan <scrajput11@gmail.com>
2022-08-05 04:01:42 +0530

    Day 5 Loops.py

---
+++ Day 5 Loops.py
@@ -0,0 +1,23 @@
+#Day 5 Loops... You will find this Code in Pyhton Terminal
+#Sooo Let's start coding
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+#Start Code
+for i in range(1, 11):
+    a = n * i
+    print(n, "x", i, "=", a)
+
+#Run this
+#This code print the table of 2
+
+#Thankyou!! I will see you on next day...
